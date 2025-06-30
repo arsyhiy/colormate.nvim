c82bb2114f55d5f735a7b8e490898788f05a2b66
arsyhiy <arsyhiy32@gmail.com>
2025-06-30 12:39:43 +0300

    more clear version without unecesery things

--- lua/my-theme/colorscheme.lua
+++ lua/my-theme/colorscheme.lua
@@ -1,81 +1,10 @@
-local hslutil = require("my-theme.hsl")
-local hsl = hslutil.hslToHex
-local util = require("my-theme.utils")
-
-
-
-
-
-
-
-local M = {}
-
---@class Palette
-M.default = {
-
-
-}
-
---@return Colorscheme
-
-
-
--- Color Palette
---@class ColorScheme: Palette
-
-
-
-
-
-
-
-
-
-
-
-
-local config = require 'my-theme.config'
 
 local colorscheme = {
   standardWhite = '#ffffff',
   standardBlack = '#282828',
 }
 
-if vim.o.background == 'light' then
-  colorscheme.editorBackground = config.transparent and 'none' or '#212121'
-  colorscheme.sidebarBackground = '#dddddd'
-  colorscheme.popupBackground = '#f6f6f6'
-  colorscheme.floatingWindowBackground = '#e0e0e0'
-  colorscheme.menuOptionBackground = '#ededed'
-  colorscheme.mainText = '#616161'
-  colorscheme.emphasisText = '#212121'
-  colorscheme.commandText = '#333333'
-  colorscheme.inactiveText = '#9e9e9e'
-  colorscheme.disabledText = '#d0d0d0'
-  colorscheme.lineNumberText = '#a1a1a1'
-  colorscheme.selectedText = '#424242'
-  colorscheme.inactiveSelectionText = '#757575'
-
-  colorscheme.windowBorder = '#c2c3c5'
-  colorscheme.focusedBorder = '#aaaaaa'
-  colorscheme.emphasizedBorder = '#999999'
-
-  colorscheme.syntaxFunction = '#6871ff'
-  colorscheme.syntaxError = '#d6656a'
-  colorscheme.syntaxKeyword = '#9966cc'
-  colorscheme.errorText = '#d32f2f'
-  colorscheme.warningText = '#f29718'
-  colorscheme.linkText = '#1976d2'
-  colorscheme.commentText = '#848484'
-  colorscheme.stringText = '#dd8500'
-  colorscheme.successText = '#22863a'
-  colorscheme.warningEmphasis = '#cd9731'
-  colorscheme.specialKeyword = '#800080'
-  colorscheme.syntaxOperator = '#a1a1a1'
-  colorscheme.foregroundEmphasis = '#000000'
-  colorscheme.terminalGray = '#333333'
-else
-  colorscheme.editorBackground = config.transparent and 'none' or '#282828'
+  colorscheme.editorBackground =  '#282828'
   colorscheme.sidebarBackground = '#1a1a1a'
   colorscheme.popupBackground = '#292929'
   colorscheme.floatingWindowBackground = '#383838'
@@ -108,6 +37,5 @@ else
   colorscheme.syntaxOperator = '#bbbbbb'
   colorscheme.foregroundEmphasis = '#ffffff'
   colorscheme.terminalGray = '#5c5c5c'
-end
 
 return colorscheme
