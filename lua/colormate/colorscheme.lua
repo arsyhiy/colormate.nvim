d7c7dc1331b0467b09487ef1810864f8d5d9a590
arsyhiy <arsyhiy32@gmail.com>
2025-07-17 15:23:53 +0300

    refactor init.lua

--- lua/colormate/colorscheme.lua
+++ lua/colormate/colorscheme.lua
@@ -1,40 +1,40 @@
 local colorscheme = {
-  standardWhite = '#d4d4d4',
-  standardBlack = '#282828',
+	standardWhite = "#d4d4d4",
+	standardBlack = "#282828",
 }
 
-  colorscheme.editorBackground =  '#1a1f14'-- background final version
-  colorscheme.sidebarBackground ='#2d381e'-- '#1d2021' side bar litarily. final version
-  colorscheme.popupBackground = '#1a1c17'-- final version
-  colorscheme.floatingWindowBackground = '#1a1c17'-- final version
-  colorscheme.menuOptionBackground = '#85adad'-- отражается на statusbar. final version
+colorscheme.editorBackground = "#1a1f14"
+colorscheme.sidebarBackground = "#2d381e"
+colorscheme.popupBackground = "#1a1c17"
+colorscheme.floatingWindowBackground = "#1a1c17"
+colorscheme.menuOptionBackground = "#85adad"
 
-  colorscheme.mainText = '#6aad49'-- '#c1c1c1' hacking green. final version
-  colorscheme.emphasisText = '#c7d1d1'
-  colorscheme.commandText = '#e0e0e0'
-  colorscheme.inactiveText = '#c7d1d1'
-  colorscheme.disabledText = '#c7d1d1'
-  colorscheme.lineNumberText = '#d1c7c7' -- номер строки кода. final version
-  colorscheme.selectedText = '#c7d1d1' -- выделенная строка кода
-  colorscheme.inactiveSelectionText = '#c7d1d1' -- final version
+colorscheme.mainText = "#6aad49"
+colorscheme.emphasisText = "#c7d1d1"
+colorscheme.commandText = "#e0e0e0"
+colorscheme.inactiveText = "#c7d1d1"
+colorscheme.disabledText = "#c7d1d1"
+colorscheme.lineNumberText = "#d1c7c7"
+colorscheme.selectedText = "#c7d1d1"
+colorscheme.inactiveSelectionText = "#c7d1d1"
 
-  colorscheme.windowBorder = '#282924' -- final version
-  colorscheme.focusedBorder = '#444444'
-  colorscheme.emphasizedBorder = '#363636'
+colorscheme.windowBorder = "#282924"
+colorscheme.focusedBorder = "#444444"
+colorscheme.emphasizedBorder = "#363636"
 
-  colorscheme.syntaxError = '#f51911'
-  colorscheme.syntaxFunction = '#79b8ff'
-  colorscheme.warningText = '#d69f1e'
-  colorscheme.syntaxKeyword = '#b392f0'
-  colorscheme.linkText = '#10e38f'
-  colorscheme.stringText = '#7d33cc' --under devolempent
-  colorscheme.warningEmphasis = '#d69f1e'
-  colorscheme.successText = '#22863a'
-  colorscheme.errorText = '#f51911'
-  colorscheme.specialKeyword = '#d3869b'
-  colorscheme.commentText = '#6b737c'
-  colorscheme.syntaxOperator = '#bbbbbb'
-  colorscheme.foregroundEmphasis = '#fbf1c7'
-  colorscheme.terminalGray = '#5c5c5c'
+colorscheme.syntaxError = "#f51911"
+colorscheme.syntaxFunction = "#79b8ff"
+colorscheme.warningText = "#d69f1e"
+colorscheme.syntaxKeyword = "#b392f0"
+colorscheme.linkText = "#10e38f"
+colorscheme.stringText = "#7d33cc"
+colorscheme.warningEmphasis = "#d69f1e"
+colorscheme.successText = "#22863a"
+colorscheme.errorText = "#f51911"
+colorscheme.specialKeyword = "#d3869b"
+colorscheme.commentText = "#6b737c"
+colorscheme.syntaxOperator = "#bbbbbb"
+colorscheme.foregroundEmphasis = "#fbf1c7"
+colorscheme.terminalGray = "#5c5c5c"
 
 return colorscheme
